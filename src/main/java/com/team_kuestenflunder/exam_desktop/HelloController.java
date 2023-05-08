3580a4fc8cd92d4b931fb1bf23f7fcfeb5a6dda0
Jan Päßler <Jan.paessler@uni-rostock.de>
2023-05-08 21:33:16 +0200

    Initial commit

---
+++ src/main/java/com/team_kuestenflunder/exam_desktop/HelloController.java
@@ -0,0 +1,14 @@
+package com.team_kuestenflunder.exam_desktop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
