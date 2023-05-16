d8578c16d8fbbf67085ebe7fd795d285894694a7
Bujhm77 <igor.malamen@web.de>
2023-05-16 16:17:48 +0200

    QuerstionsView + Controller v.1

--- src/main/java/com/team_kuestenflunder/exam_desktop/entity/Answer.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/entity/Answer.java
@@ -2,5 +2,7 @@ package com.team_kuestenflunder.exam_desktop.entity;
 
 public class Answer {
     String answerText;
-    int position;
+    boolean isRight;
+    int a;
+
 }
--- src/main/java/com/team_kuestenflunder/exam_desktop/entity/AnswerList.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/entity/AnswerList.java
@@ -1,5 +1,13 @@
 package com.team_kuestenflunder.exam_desktop.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnswerList {
+
+    static int correctAnswers; // TODO statische Methode für Erfassung der korrekten Antwortanzahl
+
+    List<Answer> answers = new ArrayList<Answer>();
+    int b;
 
 }
