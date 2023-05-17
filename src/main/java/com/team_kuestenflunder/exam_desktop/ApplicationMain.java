9cfb1887c861c6e5faab0686cfc4bbdf4f0894f8
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-05-17 21:03:15 +0200

    Scene Manager

--- src/main/java/com/team_kuestenflunder/exam_desktop/ApplicationMain.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/ApplicationMain.java
@@ -2,23 +2,15 @@ package com.team_kuestenflunder.exam_desktop;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.team_kuestenflunder.exam_desktop.controller.QuestionFormController;
-import com.team_kuestenflunder.exam_desktop.controller.SceneManager;
 import com.team_kuestenflunder.exam_desktop.moduls.DIConfigModule;
-import com.team_kuestenflunder.exam_desktop.repository.QuestionRepositoryImpl;
-import com.team_kuestenflunder.exam_desktop.services.QuestionFormServiceImpl;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class ApplicationMain extends Application {
     Injector injector = Guice.createInjector(new DIConfigModule());
     private final SceneManager sceneManager = new SceneManager();
-    private Scene formPage;
+    private Scene scene;
 
     public static void main(String[] args) {
         launch();
@@ -26,46 +18,15 @@ public class ApplicationMain extends Application {
     // Stage > Scene > Pane
     @Override
     public void start(Stage stage) throws Exception {
-       // read the fxml file with the FXMLLoader
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("questionForm.fxml"));
-//        fxmlLoader.setControllerFactory(injector::getInstance);
-//        Parent formParent = fxmlLoader.load();
-        //Create a new sceen from the root node of the fxml
-        formPage = sceneManager.getQuestionForm(stage);
+
+        scene = sceneManager.getQuestionView();
         //set a Stage Title
         stage.setTitle("Testballon ");
-        stage.setScene(formPage);
+        stage.setScene(scene);
         stage.show();
     }
 
 
 
-//    @Override
-//    public void start(Stage primaryStage) throws IOException {
-//        this.stage = primaryStage;
-//        injector = Guice.createInjector(new DIConfigModule());
-//        switchToQuestionForm();
-//        primaryStage.show();
-//    }
-//
-//    private void switchToQuestionForm() throws IOException{
-//        FXMLLoader fxmlLoader = new FXMLLoader(
-//                getClass().getResource("questionForm.fxml"));
-//        fxmlLoader.setControllerFactory(injector::getInstance);
-//        Parent root = fxmlLoader.load();
-//        Scene scene = new Scene(root);
-//        stage.setTitle("Question Form");
-//        stage.setScene(scene);
-//    }
-//
-//    private void switchToQuestionView() throws IOException{
-//        FXMLLoader fxmlLoader = new FXMLLoader(
-//                getClass().getResource("questionsView.fxml"));
-//        fxmlLoader.setControllerFactory(injector::getInstance);
-//        Parent root = fxmlLoader.load();
-//        Scene scene = new Scene(root);
-//        stage.setTitle("Question View");
-//        stage.setScene(scene);
-//    }
 
 }
---
+++ src/main/java/com/team_kuestenflunder/exam_desktop/SceneManager.java
@@ -0,0 +1,54 @@
+package com.team_kuestenflunder.exam_desktop;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.team_kuestenflunder.exam_desktop.moduls.DIConfigModule;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneManager {
+    Injector injector = Guice.createInjector(new DIConfigModule());
+    private Scene scene;
+    private Stage stage;
+    private Parent root;
+
+    public void switchSceneToQuestionForm(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("questionForm.fxml"));
+        fxmlLoader.setControllerFactory(injector::getInstance);
+        root = fxmlLoader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchSceneToQuestionView(ActionEvent event) {
+    }
+
+
+    public Scene getQuestionForm() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("questionForm.fxml"));
+        fxmlLoader.setControllerFactory(injector::getInstance);
+        Parent formParent = fxmlLoader.load();
+        //Create a new sceen from the root node of the fxml
+        return new Scene(formParent);
+    }
+
+    public Scene getQuestionView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("questionsView.fxml"));
+        fxmlLoader.setControllerFactory(injector::getInstance);
+        Parent formParent = fxmlLoader.load();
+        //Create a new sceen from the root node of the fxml
+        return new Scene(formParent);
+    }
+
+    public void SetScene(Scene scene, Stage stage) throws IOException {
+        stage.setScene(scene);
+    }
+}
