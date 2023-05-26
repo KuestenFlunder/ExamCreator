43f438d3c014e8ee2d45d748fc705ea261bd5812
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-05-26 15:37:23 +0200

    fix

--- src/main/java/com/team_kuestenflunder/exam_desktop/controller/PdfCreationPopUpController.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/controller/PdfCreationPopUpController.java
@@ -2,6 +2,8 @@ package com.team_kuestenflunder.exam_desktop.controller;
 
 import com.google.inject.Inject;
 import com.team_kuestenflunder.exam_desktop.services.PdfCreationPopUpService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PdfCreationPopUpController {
     PdfCreationPopUpService pdfCreationPopUpService;
@@ -10,4 +12,7 @@ public class PdfCreationPopUpController {
     public PdfCreationPopUpController(PdfCreationPopUpService pdfCreationPopUpService) {
         this.pdfCreationPopUpService = pdfCreationPopUpService;
     }
+
+    @FXML
+    Button bt_createExam,bt_cancel;
 }
