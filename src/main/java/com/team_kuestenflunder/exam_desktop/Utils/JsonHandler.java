92d6c8a034d4b5a3b6b2ce76abdd2b8950a465ab
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-05-31 09:50:16 +0200

    bug fix in mergeJsonFiles

--- src/main/java/com/team_kuestenflunder/exam_desktop/Utils/JsonHandler.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/Utils/JsonHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.team_kuestenflunder.exam_desktop.entity.Question;
 import javafx.collections.FXCollections;
@@ -12,7 +13,6 @@ import javafx.collections.ObservableList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -39,43 +39,23 @@ public class JsonHandler {
         }
     }
 
-
-    //TODO Add filechoser in Frontend to select the json files
     public void mergeJsonFiles(List<java.io.File> selectedFiles) {
-
-        List<JsonNode> jsonMergResults = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        ArrayNode mergedJsonNode = objectMapper.createArrayNode();
         try {
             for (File file : selectedFiles) {
-                String path = file.toString();
-                jsonMergResults = ( objectMapper.readValue(new File(path), new TypeReference<>() {
-                }));
+                JsonNode jsonNode = objectMapper.readValue(new File(String.valueOf(file)), JsonNode.class);
+                if (jsonNode.isArray()) {
+                    mergedJsonNode.addAll((ArrayNode) jsonNode);
+                } else {
+                    mergedJsonNode.add(jsonNode);
+                }
             }
-            objectMapper.writeValue(new File("src/main/Output/JsonMerge.json"), jsonMergResults);
-
+            objectMapper.writeValue(new File("src/main/Output/JsonMerge.json"), mergedJsonNode);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-    //TODO Add filechoser in Frontend to select the json files
-    public void mergeJsonFiles (String... filePaths ) {
-        List<JsonNode> jsonMergResults = new ArrayList<>();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        try {
-            for (String filePath : filePaths) {
-                jsonMergResults = objectMapper.readValue(new File(filePath), new TypeReference<>() {});
-            }
-            objectMapper.writeValue(new File("src/main/Output/mergedJson_1.json"), jsonMergResults);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        JsonHandler jsonHandler = new JsonHandler();
-        jsonHandler.mergeJsonFiles("src/main/Output/20_Questions.json","src/main/Output/JsonOutput.json");
     }
 
 }
