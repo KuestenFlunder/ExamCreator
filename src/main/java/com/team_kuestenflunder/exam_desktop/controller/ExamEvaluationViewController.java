94a18e1ac5672b94893c1dd82632703166e36a3e
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-06-05 15:30:53 +0200

    create examEvaluationTable

--- src/main/java/com/team_kuestenflunder/exam_desktop/controller/ExamEvaluationViewController.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/controller/ExamEvaluationViewController.java
@@ -3,12 +3,15 @@ package com.team_kuestenflunder.exam_desktop.controller;
 
 import com.team_kuestenflunder.exam_desktop.SceneManager;
 import com.team_kuestenflunder.exam_desktop.Utils.PDFHandler;
-import com.team_kuestenflunder.exam_desktop.entity.ExamValues;
+import com.team_kuestenflunder.exam_desktop.entity.ExamResult;
 import com.team_kuestenflunder.exam_desktop.services.ExamEvaluationViewService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,37 +24,46 @@ public class ExamEvaluationViewController {
 
     //TODO Dependency injection for both
 
-    PDFHandler pdfHandler = new PDFHandler();
 
+    ObservableList<ExamResult> examResults = FXCollections.observableArrayList();
+    @FXML
+    Button bt_backToQuestionView;
+    @FXML
+    MenuItem mi_evaluateExams, mi_evaluateExam;
+    @FXML
+    TableView<ExamResult> tv_examResults;
     //TODO dependency Injection
     public ExamEvaluationViewController() {
         this.examEvaluationViewService = new ExamEvaluationViewService();
     }
 
-    @FXML
-    Button bt_backToQuestionView;
-    @FXML
-    MenuItem mi_evaluateExams, mi_evaluateExam;
-
-    public void onBackToQuestionViewClick(ActionEvent event){
+    public void onBackToQuestionViewClick(ActionEvent event) {
         try {
             sceneManager.switchSceneToQuestionView(event);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public void onEvaluateMultiExamsClick(ActionEvent event){
 
-        List<File> examFiles =  sceneManager.addFileChooserDialogMultiple(event,"PDF","*.pdf");
-        //Stream for exam results
+    public void onEvaluateMultiExamsClick(ActionEvent event) {
 
-        System.out.println("examFiles = " + examFiles);
+        List<File> examFiles = sceneManager.addFileChooserDialogMultiple(event, "PDF", "*.pdf");
+
+        for (File file : examFiles) {
+            examResults.add(
+                    examEvaluationViewService.evaluateExam(
+                            PDFHandler.getValuesFromTest(file)));
+        }
+        examEvaluationViewService.createViewTable(tv_examResults,examResults);
+
     }
-    public void onEvaluateExamsClick(ActionEvent event){
-        File singleExam =  sceneManager.addFileChooserDialogSingle(event,"PDF","*.pdf");
 
-        ExamValues examValues = PDFHandler.getValuesFromTest(singleExam);
-        System.out.println(examEvaluationViewService.evaluateExam(examValues));
+    public void onEvaluateExamsClick(ActionEvent event) {
+        File singleExam = sceneManager.addFileChooserDialogSingle(event, "PDF", "*.pdf");
+        examResults.add(
+                examEvaluationViewService.evaluateExam(
+                        PDFHandler.getValuesFromTest(singleExam)));
+        examEvaluationViewService.createViewTable(tv_examResults, examResults);
     }
 
 }
--- src/main/java/com/team_kuestenflunder/exam_desktop/services/ExamEvaluationViewService.java
+++ src/main/java/com/team_kuestenflunder/exam_desktop/services/ExamEvaluationViewService.java
@@ -5,7 +5,12 @@ import com.team_kuestenflunder.exam_desktop.Utils.AlertMessage;
 import com.team_kuestenflunder.exam_desktop.entity.ExamResult;
 import com.team_kuestenflunder.exam_desktop.entity.ExamValues;
 import com.team_kuestenflunder.exam_desktop.repository.QuestionRepositoryImpl;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -15,7 +20,7 @@ public class ExamEvaluationViewService {
 
     QuestionRepositoryImpl questionRepository;
 
-//TODO Add Injection
+    //TODO Add Injection
     public ExamEvaluationViewService() {
         this.questionRepository = new QuestionRepositoryImpl();
     }
@@ -43,7 +48,7 @@ public class ExamEvaluationViewService {
 
         String testResult = percentageOfCorrectAnswers >= 65 ? "Bestanden" : "Nicht bestanden";
 
-        return new ExamResult (
+        return new ExamResult(
                 examValues.getStudentName(),
                 examValues.getStudentSurname(),
                 examValues.getDateOfTest(),
@@ -51,14 +56,14 @@ public class ExamEvaluationViewService {
                 percent,
                 testResult,
                 resultList
-                );
+        );
     }
 
     private boolean evaluateSingleQuestion(ExamValues examValues, int i) {
         int counter = 0;
         for (int j = 0; j < 6; j++) {
-            String studentAnswer = examValues.getAnswer_Map().get("AnswerBox_" + i  + j);
-            String rightAnswer = examValues.getAnswer_Map().get("CorrectAnswerBox_" + i  + j);
+            String studentAnswer = examValues.getAnswer_Map().get("AnswerBox_" + i + j);
+            String rightAnswer = examValues.getAnswer_Map().get("CorrectAnswerBox_" + i + j);
             if (studentAnswer.equals(rightAnswer)) {
                 counter++;
             }
@@ -66,8 +71,57 @@ public class ExamEvaluationViewService {
         return counter == 6;
     }
 
+    public void createViewTable(TableView<ExamResult> tableView, ObservableList<ExamResult> examResults) {
+        TableColumn<ExamResult, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("studentName"));
+
+        TableColumn<ExamResult, String> surnameColumn = new TableColumn<>("Nachname");
+        surnameColumn.setCellValueFactory(new PropertyValueFactory<>("studentSurname"));
+
+        TableColumn<ExamResult,String> dateColumn = new TableColumn<>("Prüfungsdatum");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfExam"));
+
+        TableColumn<ExamResult,Integer> numberOfQuestionsColumn = new TableColumn<>("Anzahl der Fragen");
+        numberOfQuestionsColumn.setCellValueFactory(new PropertyValueFactory<>("numberOfQuestions"));
+
+        TableColumn<ExamResult,String> percentColumn = new TableColumn<>("Erreichte Prozent");
+        percentColumn.setCellValueFactory(new PropertyValueFactory<>("percent"));
+
+        TableColumn<ExamResult,String> examResultColumn = new TableColumn<>("Ergebnis");
+        examResultColumn.setCellValueFactory(new PropertyValueFactory<>("examResult"));
+        examResultColumn.setCellFactory(column -> new TableCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (item == null || empty) {
+                    setText(null);
+                    setStyle("");
+                } else {
+                    setText(item);
+                    // Style the cell's background based on the exam result
+                    if (item.equals("Bestanden")) {
+                        // Green background if "Bestanden"
+                        setStyle("-fx-background-color: lightgreen");
+                    } else if (item.equals("Nicht bestanden")) {
+                        // Red background if "Nicht bestanden"
+                        setStyle("-fx-background-color: tomato");
+                    } else {
+                        // Default
+                        setStyle("");
+                    }
+                }
+            }
+        });
 
 
+        tableView.setItems(examResults);
 
+        // Clear the existing columns
+        tableView.getColumns().clear();
+
+        // Add the columns
+        tableView.getColumns().addAll(nameColumn, surnameColumn, dateColumn, numberOfQuestionsColumn, percentColumn, examResultColumn);
+    }
 
 }
